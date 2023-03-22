b11514f8c59c28e7336602feb8cfd09b3fb4ca49
brunocaike <brunokf1512@gmail.com>
2023-03-22 00:19:40 -0300

    feito a tela de erro 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,19 @@
+import { Link } from 'react-router-dom';
 import Base from '../components/Base';
+import { routes } from '../utils/variables';
 
 const NotFound = () =>  (
     <Base>
-        <h1>Página não encontrada</h1>
+        <section className="s-error">
+            <div className="container">
+                <header>
+                    <h1>Página não encontrada</h1>
+                    <p>A URL solicitada não pôde ser encontrada. <br />Por favor verifique o endereço ou informe o administrador.</p>
+                </header>
+                <Link to={routes.home}>Voltar para início</Link>
+            </div>
+        </section>
+        
     </Base>
 );
   
